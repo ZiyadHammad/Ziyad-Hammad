3bb9fe3340a0ce18857a5064f23197265f711e75
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-03-03 21:26:38 -0500

    Components and Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
 import GlobalStyle from "./globalStyles"
+
+// Components
+import Main from "./components/Main"
+import About from "./components/About"
+import Blog from "./components/Blog"
+import Work from "./components/Work"
+import Skills from "./components/Skills"
+
+
+
 function App() {
   return <>
     <GlobalStyle />
-    App File
+
+    <ThemeProvider theme={lightTheme} >
+     <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route  path="/about" element={<About />} />
+        <Route  path="/blog" element={<Blog />} />
+        <Route  path="/work" element={<Work />} />
+        <Route  path="/skills" element={<Skills />} />
+    </Routes>
+      
+    </ThemeProvider>
+    
     </>
     
 }
